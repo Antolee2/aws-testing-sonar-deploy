8c1ebf87fb1635db3c75c23d98023f2d179886c8
Antonlee <leeroychivandire2022@gmail.com>
2026-02-28 12:36:21 +0530

    second commit

--- aws-testing-deploy/src/test/java/com/example/demo/cseControllerTest.java
+++ aws-testing-deploy/src/test/java/com/example/demo/cseControllerTest.java
@@ -1,14 +1,17 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class cseControllerTest {
+	@Autowired
+	
 	cseController c = new cseController();
 	@Test
 	void csetest() {
 		int result=c.addcse(2,3);
-		assertEquals (5,result);
+		assertEquals(5,result);
 	}
 
 }
